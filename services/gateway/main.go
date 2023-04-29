8ee98e6b50d9e5201ec5a5ed47f60357a4234857
Denis <shumkovdenis@gmail.com>
2023-04-29 13:37:49 +0700

    feat(gateway/main.go): add support for PORT environment variable to be able to run app on a configurable port. Add a simple GET route to test the app.

---
+++ services/gateway/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/caarlos0/env/v8"
+	"github.com/gofiber/fiber/v2"
+)
+
+type config struct {
+	Port int `env:"PORT" envDefault:"6000"`
+}
+
+func main() {
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil {
+		panic(err)
+	}
+
+	// client, err := dapr.NewClient()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer client.Close()
+
+	// sessionService := services.NewDaprSessionStore(client, "statestore")
+	// betService := services.NewDaprBetService(client)
+	// historyService := services.NewDaprHistoryService(client, "history-pubsub", "history")
+	// command := services.NewCommandService(sessionService, betService, historyService)
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	if err := app.Listen(fmt.Sprintf(":%d", cfg.Port)); err != nil {
+		panic(err)
+	}
+}
