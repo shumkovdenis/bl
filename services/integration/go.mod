0039d607c1ad83db8de3b7b725057f144908540f
Denis <shumkovdenis@gmail.com>
2023-05-02 17:21:31 +0700

    feat(integration): add Dockerfile and main.go for integration service
    This commit adds a Dockerfile and main.go file for the integration service. The Dockerfile is used to build the application and copy it into a base image. The main.go file contains the implementation of the integration service, which listens on a configurable port and handles requests for the GetBalance method. The implementation uses the connect-go library to handle requests and responses.

---
+++ services/integration/go.mod
@@ -0,0 +1,15 @@
+module github.com/shumkovdenis/services/integration
+
+go 1.20
+
+require (
+	github.com/bufbuild/connect-go v1.7.0
+	github.com/caarlos0/env/v8 v8.0.0
+	github.com/shumkovdenis/bl v0.0.0-20230429104538-2454cdde604c
+	golang.org/x/net v0.9.0
+)
+
+require (
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ services/integration/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/bufbuild/connect-go"
+	"github.com/caarlos0/env/v8"
+	integration "github.com/shumkovdenis/bl/gen/integration/v1"
+	integrationConnect "github.com/shumkovdenis/bl/gen/integration/v1/integrationv1connect"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+type Config struct {
+	Port int `env:"PORT" envDefault:"6000"`
+}
+
+type Server struct{}
+
+func (s *Server) GetBalance(
+	ctx context.Context,
+	req *connect.Request[integration.GetBalanceRequest],
+) (*connect.Response[integration.GetBalanceResponse], error) {
+	res := connect.NewResponse(&integration.GetBalanceResponse{
+		Balance: 100500,
+	})
+	res.Header().Set("integration-version", "v1")
+
+	return res, nil
+}
+
+func main() {
+	cfg := Config{}
+	opts := env.Options{RequiredIfNoDef: true}
+	if err := env.ParseWithOptions(&cfg, opts); err != nil {
+		panic(err)
+	}
+
+	server := &Server{}
+
+	mux := http.NewServeMux()
+	mux.Handle(integrationConnect.NewIntegrationServiceHandler(server))
+
+	if err := http.ListenAndServe(
+		fmt.Sprintf(":%d", cfg.Port),
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(mux, &http2.Server{}),
+	); err != nil {
+		panic(err)
+	}
+}
