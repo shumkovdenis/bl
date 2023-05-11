8e8c07c3cdca7d26ecf2a8aa64c6be825270ef3e
Denis <shumkovdenis@gmail.com>
2023-05-11 11:09:30 +0700

    feat(utils.go): add SpanContextFromBinary function to parse binary data into a trace.SpanContext object fix(utils.go): change function call from utils.SpanContextFromBinary to SpanContextFromBinary to match function name

--- services/integration/helpers/utils.go
+++ services/integration/helpers/utils.go
@@ -35,11 +35,36 @@ func newSpanID() trace.SpanID {
 	return sid
 }
 
+func SpanContextFromBinary(b []byte) (sc trace.SpanContext, ok bool) {
+	var scConfig trace.SpanContextConfig
+	log.Println("SpanContextFromBinary", len(b), b[0])
+	if len(b) == 0 || b[0] != 0 {
+		return trace.SpanContext{}, false
+	}
+	b = b[1:]
+	log.Println("SpanContextFromBinary", len(b), b[0])
+	if len(b) >= 17 && b[0] == 0 {
+		copy(scConfig.TraceID[:], b[1:17])
+		b = b[17:]
+	} else {
+		return trace.SpanContext{}, false
+	}
+	if len(b) >= 9 && b[0] == 1 {
+		copy(scConfig.SpanID[:], b[1:9])
+		b = b[9:]
+	}
+	if len(b) >= 2 && b[0] == 2 {
+		scConfig.TraceFlags = trace.TraceFlags(b[1])
+	}
+	sc = trace.NewSpanContext(scConfig)
+	return sc, true
+}
+
 func setNewGRPCTraceHeaderFromContext(header HeaderSetter, ctx context.Context) {
 	value := ExtractGRPCTraceBin(ctx)
 	log.Println("ExtractGRPCTraceBin", value)
 	if value != "" {
-		sc, ok := utils.SpanContextFromBinary([]byte(value))
+		sc, ok := SpanContextFromBinary([]byte(value))
 		log.Println("SpanContextFromBinary", sc, ok)
 		if ok {
 			newsc := sc.WithSpanID(newSpanID())
