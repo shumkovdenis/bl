189c4e7512a201812136351fd906634b81d246e1
Denis <shumkovdenis@gmail.com>
2023-05-16 16:16:42 +0700

    fix(http_callee.go): add error handling for unexpected status codes in http response to improve error reporting

--- http_callee.go
+++ http_callee.go
@@ -54,6 +54,10 @@ func (c httpCallee) Call(ctx context.Context, msg Message) (Message, error) {
 	}
 	defer res.Body.Close()
 
+	if res.StatusCode >= 200 && res.StatusCode < 300 {
+		return Message{}, fmt.Errorf("unexpected status code: %d", res.StatusCode)
+	}
+
 	b, err := io.ReadAll(res.Body)
 	if err != nil {
 		return Message{}, fmt.Errorf("failed to read response body: %w", err)
