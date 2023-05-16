2a4f4fc984a456f1eae929c3c957aa6c7e0e71c0
Denis <shumkovdenis@gmail.com>
2023-05-16 09:29:53 +0700

    feat(main.go): add support for connect mode to start a connect server instead of http server based on configuration refactor(main.go): refactor main function to improve readability and maintainability

--- main.go
+++ main.go
@@ -15,11 +15,21 @@ func main() {
 
 	if cfg.Callee.Mode == "http" {
 		callee = NewHTTPCallee(cfg)
+	} else if cfg.Callee.Mode == "connect" {
+		callee = NewConnectCallee(cfg)
 	} else {
 		callee = NewFakeCallee()
 	}
 
-	if err := NewHTTPService(cfg, callee); err != nil {
-		log.Fatal("failed to start http server:", err)
+	if cfg.Mode == "http" {
+		if err := NewHTTPService(cfg, callee); err != nil {
+			log.Fatal("failed to start http server:", err)
+		}
+	} else if cfg.Mode == "connect" {
+		if err := NewConnectService(cfg, callee); err != nil {
+			log.Fatal("failed to start connect server:", err)
+		}
+	} else {
+		log.Fatal("unknown mode")
 	}
 }
